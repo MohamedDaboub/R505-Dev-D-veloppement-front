ade43ab4d91a1db9c17e21ec4c73b1ae8cbeb5be
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2023-09-26 11:14:41 +0200

    correction du code

--- js/exercice.js
+++ js/exercice.js
@@ -116,51 +116,85 @@ let chaussures = [
   
 // 1. Faire une boucle sur tout le tableau et de log les chaussures une par une
 console.log("Toutes les chaussures");
-for (let i = 0; i < chaussures.length; i++) {
-    console.log(chaussures[i]);
-} 
+// for (let i = 0; i < chaussures.length; i++) {
+//     console.log(chaussures[i]);
+// } 
+chaussures.forEach(chaussures => {
+    console.log(chaussures);
+});
 // 2. Log un tableau de toutes les Nikes
 console.log("Nike");
-for (let i = 0; i < chaussures.length; i++) {
-    if (chaussures[i].marque == "Nike") {
-        console.log(chaussures[i]);
+// for (let i = 0; i < chaussures.length; i++) {
+//     if (chaussures[i].marque == "Nike") {
+//         console.log(chaussures[i]);
+//     }
+// }
+chaussures.find(chaussures => {
+    if (chaussures.marque == "Nike") {
+        console.log(chaussures);
     }
-}
+});
 // 3. Log du prix des Vans "Old Skool"
 console.log("Prix de la Vans Old Skool");
-for (let i = 0; i < chaussures.length; i++) {
-    if (chaussures[i].titre == "Old Skool") {
-        console.log(chaussures[i].prix);
+// for (let i = 0; i < chaussures.length; i++) {
+//     if (chaussures[i].titre == "Old Skool") {
+//         console.log(chaussures[i].prix);
+//     }
+// }
+chaussures.find(chaussures => {
+    if (chaussures.titre == "Old Skool") {
+        console.log(chaussures.prix);
     }
-}
+});
 // 4. Log un tableau de toutes les chaussures collector
 console.log("Toutes les chaussures collector");
-for (let i = 0; i< chaussures.length; i++){
-    if (chaussures[i].collector == true){
-        console.log(chaussures[i]);
+// for (let i = 0; i< chaussures.length; i++){
+//     if (chaussures[i].collector == true){
+//         console.log(chaussures[i]);
+//     }
+// }
+chaussures.find(chaussures => {
+    if (chaussures.collector == true) {
+        console.log(chaussures);
     }
-}
+});
 // 5. Log couleur lacets de la vans Sk8
 console.log("Couleur lacets de la vans Sk8");
-for (let i=0; i< chaussures.length; i++){
-    if (chaussures[i].titre == "Sk8"){
-        console.log(chaussures[i].elements.lacets);
+// for (let i=0; i< chaussures.length; i++){
+//     if (chaussures[i].titre == "Sk8"){
+//         console.log(chaussures[i].elements.lacets);
+//     }
+// }  
+chaussures.find(chaussures => {
+    if (chaussures.titre == "Sk8") {
+        console.log(chaussures.elements.lacets);
     }
-}  
+});
 // 6. Changer la couleur de la vans Sk8 en violet et log la chaussure
 console.log("Changement de couleur de la vans Sk8");
-for (let i=0; i< chaussures.length; i++){
-    if (chaussures[i].titre == "Sk8"){
-        chaussures[i].elements.lacets = "violet";
-        console.log(chaussures[i]);
+// for (let i=0; i< chaussures.length; i++){
+//     if (chaussures[i].titre == "Sk8"){
+//         chaussures[i].elements.lacets = "violet";
+//         console.log(chaussures[i]);
+//     }
+// } 
+chaussures.forEach(chaussures => {
+    if (chaussures.titre == "Sk8") {
+        chaussures.elements.lacets = "violet";
+        console.log(chaussures);
     }
-} 
+});
 // 7. Plus compliqué : Log le montant total de Toutes les Nike
 console.log("Montant total de toutes les Nike");
 let montant_Total = 0;
-for (let i=0; i< chaussures.length; i++){
-    if (chaussures[i].marque == "Nike"){
-        montant_Total += chaussures[i].prix;
+// for (let i=0; i< chaussures.length; i++){
+//     if (chaussures[i].marque == "Nike"){
+//         montant_Total += chaussures[i].prix;
+//     }
+// }
+chaussures.forEach(chaussures => {
+    if (chaussures.marque == "Nike") {
+        montant_Total += chaussures.prix;
     }
-}
+});
 console.log(`Le montant Total de Tous les chaussers Nike est`,montant_Total);
